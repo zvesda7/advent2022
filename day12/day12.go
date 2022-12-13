48edc0ef0f8c798d305e25f8740eef8bef7eb04a
Vernon Gilbert <zvesda7@gmail.com>
2022-12-12 22:48:16 -0600

    switch to bfs from dijkstra, cleanup

--- day12/day12.go
+++ day12/day12.go
@@ -11,96 +11,65 @@ func readLines(path string) ([]string, error) {
 	return strings.Split(string(file), "\n"), nil
 }
 
-const inf = 9999
-
 type Node struct {
-	x       int
-	y       int
-	z       int
-	dist    int
-	visited bool
+	x    int
+	y    int
+	z    int
+	dist int
 }
 
-func pHash(x, y int) int {
-	return x*10000 + y
+func (n Node) GetHash() int {
+	return n.x*10000 + n.y
 }
 
-func getZ(instr *[]string, n *Node) byte {
-	return byte((*instr)[n.y][n.x])
-}
-
-func getAdj(allNodes *map[int]*Node, n *Node, dir int, reverseHeightRule bool) *Node {
-	x := n.x
-	y := n.y
-	switch dir {
-	case 0:
-		x++
-	case 1:
-		x--
-	case 2:
-		y++
-	case 3:
-		y--
-	}
-	if nt, found := (*allNodes)[pHash(x, y)]; found {
-		if reverseHeightRule {
-			if nt.z >= (n.z - 1) {
-				return nt
-			}
-		} else {
-			if nt.z <= (n.z + 1) {
-				return nt
-			}
-		}
-	}
-	return nil
-}
-
-func getAllAdj(allNodes *map[int]*Node, n *Node, reverseHeightRule bool) []*Node {
+func getAllAdj(grid *[][]*Node, n *Node) []*Node {
 	var nodes []*Node
-	for dir := 0; dir < 4; dir++ {
-		if nt := getAdj(allNodes, n, dir, reverseHeightRule); nt != nil {
-			nodes = append(nodes, nt)
+	var deltas = [4][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+	for _, delta := range deltas {
+		x, y := n.x+delta[0], n.y+delta[1]
+		if y >= 0 && y < len(*grid) && x >= 0 && x < len((*grid)[y]) {
+			nt := (*grid)[y][x]
+			if nt.z >= (n.z - 1) { //reverse logic, we only traverse end to start to make part 2 easier
+				nodes = append(nodes, nt)
+			}
 		}
 	}
 	return nodes
 }
 
-func findShortestPath(allNodes *map[int]*Node, startN *Node, endN *Node, endOnAnyOfSameZAsEnd bool, reverseHeightRule bool) int {
-	for (*allNodes)[pHash(endN.x, endN.y)].dist == inf {
-		var currNode *Node
-		for _, n := range *allNodes {
-			if n.visited == false && (currNode == nil || n.dist < currNode.dist) {
-				currNode = n
-			}
-		}
-
-		adjNodes := getAllAdj(allNodes, currNode, reverseHeightRule)
-		nextDist := currNode.dist + 1
+func findShortestPath(grid *[][]*Node, startN *Node, endN *Node, endOnAnyOfSameZAsEnd bool) int {
+	//BFS
+	var workingSet []*Node
+	workingSet = append(workingSet, startN)
+	visited := map[int]bool{}
+	for len(workingSet) > 0 {
+		var currNode *Node = workingSet[0]
+		workingSet = workingSet[1:]
+		adjNodes := getAllAdj(grid, currNode)
 		for _, n := range adjNodes {
-			if nextDist < n.dist {
-				n.dist = nextDist
+			if !visited[n.GetHash()] {
+				n.dist = currNode.dist + 1
+				workingSet = append(workingSet, n)
+				visited[n.GetHash()] = true
 			}
 		}
-		currNode.visited = true
 		if endOnAnyOfSameZAsEnd && currNode.z == endN.z {
 			return currNode.dist
 		}
-		delete(*allNodes, pHash(currNode.x, currNode.y))
-		//fmt.Println(currNode)
 	}
 	return endN.dist
 }
 
-func loadNodes(instr []string) (map[int]*Node, *Node, *Node) {
+func loadGrid(instr []string) ([][]*Node, *Node, *Node) {
 	var startN *Node
 	var endN *Node
-	allNodes := make(map[int]*Node) //key is node hash, value is distance to reach it.
+	grid := make([][]*Node, len(instr))
 	for y, row := range instr {
+		grid[y] = make([]*Node, len(row))
 		for x, cell := range row {
 			z := int(cell)
-			thisNode := Node{x, y, z, inf, false}
-			allNodes[pHash(thisNode.x, thisNode.y)] = &thisNode
+			thisNode := Node{x, y, z, 0}
+			grid[thisNode.y][thisNode.x] = &thisNode
 			if cell == 'S' {
 				startN = &thisNode
 				startN.dist = 0
@@ -111,7 +80,7 @@ func loadNodes(instr []string) (map[int]*Node, *Node, *Node) {
 			}
 		}
 	}
-	return allNodes, startN, endN
+	return grid, startN, endN
 }
 
 func main() {
@@ -121,14 +90,12 @@ func main() {
 		os.Exit(0)
 	}
 
-	allNodes, startN, endN := loadNodes(instr)
-	part1 := findShortestPath(&allNodes, startN, endN, false, false)
+	grid, startN, endN := loadGrid(instr)
+	part1 := findShortestPath(&grid, endN, startN, false)
 	fmt.Println("part1", part1)
 
 	//walk backward, looking for any 'a'
-	allNodes2, startN2, endN2 := loadNodes(instr)
-	startN2.dist = inf
-	endN2.dist = 0
-	part2 := findShortestPath(&allNodes2, endN2, startN2, true, true)
+	grid, startN, endN = loadGrid(instr)
+	part2 := findShortestPath(&grid, endN, startN, true)
 	fmt.Println("part2", part2)
 }
