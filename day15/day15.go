3beb205f141017efd6df0d7789c8382884a4c882
Vernon Gilbert <zvesda7@gmail.com>
2022-12-15 21:31:16 -0600

    day 15 solve

--- day15/day15.go
+++ day15/day15.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -13,11 +14,27 @@ func readLines(path string) ([]string, error) {
 	return strings.Split(string(file), "\n"), nil
 }
 
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func remove(slice []Range, s int) []Range {
+	return append(slice[:s], slice[s+1:]...)
+}
+
 type Sensor struct {
 	x       int
 	y       int
 	beaconX int
 	beaconY int
+}
+
+type Range struct {
+	x     int
+	count int
 }
 
 func parseSensors(instr []string) []Sensor {
@@ -35,13 +52,92 @@ func parseSensors(instr []string) []Sensor {
 	return sensors
 }
 
+func calcManhat(s Sensor) int {
+	return abs(s.x-s.beaconX) + abs(s.y-s.beaconY)
+}
+
+func reduceRanges(r []Range) []Range {
+	//combine ranges until they no longer overlap, then sum them
+	sort.Slice(r, func(i, j int) bool {
+		return r[i].x < r[j].x
+	})
+	for i := 0; i < len(r); i++ {
+
+		for j := i + 1; j < len(r); {
+			if r[j].x <= r[i].x+r[i].count {
+				newCount := r[j].x - r[i].x + r[j].count
+				if newCount > r[i].count {
+					r[i].count = newCount
+				}
+				r = remove(r, j)
+			} else {
+				break
+			}
+		}
+	}
+	return r
+}
+
+func findRangesAtRow(sensors []Sensor, y int) []Range {
+	ranges := make([]Range, 0)
+	for _, s := range sensors {
+		d := calcManhat(s)
+		d2y := abs(s.y - y)
+		if d2y <= d {
+			dx := d - d2y
+			ranges = append(ranges, Range{s.x - dx, dx*2 + 1})
+		}
+	}
+	return reduceRanges(ranges)
+}
+
+func boxRanges(r []Range, boxLen int) []Range {
+	for i := len(r) - 1; i >= 0; i-- {
+		if r[i].x < 0 {
+			r[i].count += r[i].x
+			r[i].x = 0
+		}
+		if r[i].x+r[i].count > boxLen {
+			r[i].count += boxLen - (r[i].x + r[i].count)
+		}
+		if r[i].count < 0 {
+			r = remove(r, i)
+		}
+	}
+	return r
+}
+
 func main() {
-	var instr, err = readLines("test.txt")
+	const TestRow = 2000000
+	const BoxLen = 4000001
+	var instr, err = readLines("input.txt")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
 
 	sensors := parseSensors(instr)
-	fmt.Println(sensors)
+
+	beaconXs := make(map[int]bool)
+	for _, s := range sensors {
+		if s.beaconY == TestRow {
+			beaconXs[s.beaconX] = true
+		}
+	}
+	ranges := findRangesAtRow(sensors, TestRow)
+	allPoints := 0
+	for _, x := range ranges {
+		allPoints += x.count
+	}
+	fmt.Println("Part 1", allPoints-len(beaconXs))
+
+	for y := 0; y < BoxLen; y++ {
+		ranges := findRangesAtRow(sensors, y)
+		ranges = boxRanges(ranges, BoxLen)
+		if len(ranges) == 2 {
+			x := ranges[1].x - 1
+			fmt.Println("Part 2", 4000000*x+y)
+			break
+		}
+	}
 }
